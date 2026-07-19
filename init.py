7f441e2f1fb278231f7f089e13fdfd302604af9a
Bahamas93 <patrickhiebner@hotmail.com>
2026-07-19 13:38:13 +0200

    init.py

--- init.py
+++ init.py
@@ -1,6 +1,9 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN, CONF_GKZ
+from .coordinator import GeoSphereWarnCoordinator
+
 PLATFORMS = ["sensor"]
 
 
@@ -8,6 +11,16 @@ async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
 ) -> bool:
+
+    coordinator = GeoSphereWarnCoordinator(
+        hass,
+        entry.data[CONF_GKZ],
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(
         entry,
@@ -22,7 +35,12 @@ async def async_unload_entry(
     entry: ConfigEntry,
 ) -> bool:
 
-    return await hass.config_entries.async_unload_platforms(
+    unload_ok = await hass.config_entries.async_unload_platforms(
         entry,
         PLATFORMS,
     )
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
