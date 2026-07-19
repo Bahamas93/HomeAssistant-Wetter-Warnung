4ae343f8d1551a5ca58b85456eda9071e2b31eaa
Bahamas93 <patrickhiebner@hotmail.com>
2026-07-19 13:36:18 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,14 @@
+DOMAIN = "geosphere_warn"
+
+CONF_GKZ = "gkz"
+
+SCAN_INTERVAL = 300
+
+API_URL = "https://warnungen.zamg.at/wsapp/api/getWarnstatus"
+
+LEVELS = {
+    0: "green",
+    1: "yellow",
+    2: "orange",
+    3: "red",
+}
